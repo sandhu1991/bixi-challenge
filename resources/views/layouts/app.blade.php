01294cdad6a9ae6bcc170faf4ed7563c407c48a4
hardilpreet <hardilpreet.singh@workland.com>
2021-07-05 20:07:06 -0400

    pull

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,6 @@
         @yield('content')
     </main>
   </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js', true) }}"></script>
 </body>
 </html>
